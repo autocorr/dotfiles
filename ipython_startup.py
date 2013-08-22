5500afc37b0d372e2778bf3f57c097bda5a98dce
Brian Svoboda <bones253@gmail.com>
2013-08-21 17:42:23 -0700

    Add future division and warning

--- ipython_startup.py
+++ ipython_startup.py
@@ -5,13 +5,15 @@ try:
     import besl
 except ImportError:
     import sys
+    import warnings
     scripts_dir = '/mnt/eld_data/scripts/'
     paths = ['besl', 'py']
     paths = [scripts_dir + path for path in paths]
     sys.path.extend(paths)
-    print 'PYTHONPATH set incorrectly'
+    warnings.warn('PYTHONPATH set incorrectly')
 
 # imports
+from __future__ import division
 import besl
 import numpy as np
 import pandas as pd
