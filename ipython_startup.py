904e181bd796d2659b414b4c889a88d3f4b70a2a
Brian Svoboda <bones253@gmail.com>
2013-09-20 14:59:55 -0700

    IPython to python 3 compat

--- ipython_startup.py
+++ ipython_startup.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
+# encoding: utf-8
 
-# path for local packages
+## Python 3 compatibility
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+try:
+    from future import standard_library
+    from future.builtins import *
+except ImportError:
+    import warnings
+    warnings.warn('future not installed, not using PY3 compatibility.')
+
+## Import local modules if pathes incorrectly set
 try:
     import besl
 except ImportError:
@@ -11,13 +22,15 @@ except ImportError:
     paths = [scripts_dir + path for path in paths]
     sys.path.extend(paths)
     warnings.warn('PYTHONPATH set incorrectly')
+    import besl
 
-# imports
-from __future__ import division
-import besl
+## Import aliases
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from astropy.io import fits
+from astropy import wcs
 
-# matplotlib
+## Matplotlib specific settings
+# Turn plot interactivity on by default
 plt.ion()
