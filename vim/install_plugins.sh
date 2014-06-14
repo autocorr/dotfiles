c467a0d26bb33c96892bf706f42c0a83d9cb0789
Brian Svoboda <svobodb@email.arizona.edu>
2014-06-14 15:38:57 -0700

    Add install plugin script

---
+++ vim/install_plugins.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd ~/.vim/bundle
+git clone https://github.com/jiangmiao/auto-pairs
+git clone https://github.com/vim-scripts/tComment
+git clone https://github.com/kien/ctrlp.vim
+git clone https://github.com/davidhalter/jedi-vim
+git clone https://github.com/scrooloose/syntastic
+git clone https://github.com/bling/vim-airline
+git clone https://github.com/tpope/vim-fugitive
+git clone https://github.com/jpalardy/vim-slime
+git clone https://github.com/tpope/vim-surround
