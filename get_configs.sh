1d0c09f850a1002d0a36db4db406ff031547836b
Brian Svoboda <svobodb@email.arizona.edu>
2014-06-24 15:29:58 -0700

    remove get configs

--- get_configs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cp -f $HOME/.bashrc ./bashrc
-cp -f $HOME/.vimrc ./vim/vimrc
-cp -f $HOME/.zhsrc ./zsh/zshrc
-cp -f $HOME/.tmux.conf ./tmux.conf
-cp -f $HOME/.gitconfig ./gitconfig
