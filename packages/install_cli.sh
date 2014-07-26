1076bb86bce5ba99a3d351f66eadd73a152b5927
Brian Svoboda <svobodb@email.arizona.edu>
2014-07-26 13:02:48 -0700

    Add a few programs

--- packages/install_cli.sh
+++ packages/install_cli.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install cmus git htop irssi ranger redshift rtorrent tig tmux vim zsh
+sudo apt-get install cmus git htop irssi ranger redshift rtorrent tig tmux vim zsh build-essential unclutter
 echo ":: 'julia' requires manual install"
 echo ":: packages not selected: mutt"
