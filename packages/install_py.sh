5a9c1cf44b95d7a3175581fba5bcdc5f0467e8b8
Brian Svoboda <svobodb@email.arizona.edu>
2014-07-28 08:14:08 -0700

    Update install for pip

--- packages/install_py.sh
+++ packages/install_py.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update
 sudo apt-get upgrade
-sudo python easy_install pip
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
 sudo pip install grin ipdb ipython matplotlib nose numexpr numpy pandas pypi-cli scipy astropy aplpy
