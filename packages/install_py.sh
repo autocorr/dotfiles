4bb0d9cde034ab978e37e744b748731594ce0f83
Brian Svoboda <svobodb@email.arizona.edu>
2014-07-28 22:31:34 -0700

    Format install notes

--- packages/install_py.sh
+++ packages/install_py.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get update
 sudo apt-get upgrade
+sudo apt-get install python3
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
-sudo pip install grin ipdb ipython matplotlib nose numexpr numpy pandas pypi-cli scipy astropy aplpy jedi
+sudo pip install grin ipdb ipython matplotlib nose numexpr numpy pandas pypi-cli scipy scikit-learn astropy aplpy emcee jedi virtualenv
