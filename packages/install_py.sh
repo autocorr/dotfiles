4921f7c61c0bebde6705ca2fa0171a206fbc98e5
Brian Svoboda <svobodb@email.arizona.edu>
2014-09-23 16:17:02 -0700

    updates to programs

--- packages/install_py.sh
+++ packages/install_py.sh
@@ -4,4 +4,6 @@ sudo apt-get upgrade
 sudo apt-get install python3
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
+# dependencies for matplotlib
+sudo apt-get install libagg-dev libjpeg8-dev libfreetype6-dev libgpng12-dev
 sudo pip install grin ipdb ipython matplotlib nose numexpr numpy pandas pypi-cli scipy scikit-learn astropy aplpy emcee jedi virtualenv sphinx
