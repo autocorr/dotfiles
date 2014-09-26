aa45a1326f61cfc2236b9a9f08b693bfb1d01a67
Brian Svoboda <svobodb@email.arizona.edu>
2014-09-25 17:59:56 -0700

    Add mplayer

--- packages/install_cli.sh
+++ packages/install_cli.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install cmus git htop irssi ranger redshift rtorrent tig tmux vim zsh build-essential unclutter trash-cli sshfs python-dev
+sudo apt-get install cmus git htop irssi ranger redshift rtorrent tig tmux vim zsh build-essential unclutter trash-cli sshfs python-dev mplayer
 echo ":: 'julia' requires manual install"
 echo ":: packages not selected: mutt"
