ae4d95dbf49d4857bb4c25d94fbd23b00cb21e02
Brian Svoboda <svobodb@email.arizona.edu>
2014-11-05 16:06:41 -0700

    Add syntax highlighting to zsh

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -2,5 +2,6 @@
 
 cp ./zshrc $HOME/.zshrc
 git clone https://github.com/robbyrussell/oh-my-zsh $HOME/.oh-my-zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
 cp ./quetzal.zsh-theme $HOME/.oh-my-zsh/themes/
 chsh -s /bin/zsh $USER
