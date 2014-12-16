4d5219c0c0848d250af8d202cf74ae1b449ecbde
Brian Svoboda <svobodb@email.arizona.edu>
2014-12-16 12:04:31 -0700

    add haskell install file

---
+++ packages/install_hs.sh
@@ -0,0 +1,11 @@
+sudo apt-get install ghc cabal-install
+sudo apt-get install libghc-bzlib-dev libghc-haskell-src-dev
+chmod g-w ~/.ghc
+cp ../ghci.conf ~/.ghc/ghci.conf
+chmod g-w ~/.ghc/ghci.conf
+cabal update
+cabal install hscolour ipprint ghci-pretty vector
+cabal install happy  # dependency for hoogle
+source ~/.zshrc
+cabal install hoogle
+hoogle data
