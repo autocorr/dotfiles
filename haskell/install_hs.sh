4487690cd4be49cf44f29cdebc1459d664c8a4f8
Brian Svoboda <svobodb@email.arizona.edu>
2014-12-18 16:20:45 -0700

    xmonad updates

--- haskell/install_hs.sh
+++ haskell/install_hs.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 sudo apt-get install ghc hdevtools cabal-install
 sudo apt-get install libghc-bzlib-dev libghc-haskell-src-dev
-chmod g-w ~/.ghc
 cp ../ghci.conf ~/.ghc/ghci.conf
-chmod g-w ~/.ghc/ghci.conf
 cabal update
 cabal install hscolour ipprint ghci-pretty vector
 cabal install happy  # dependency for hoogle
 source ~/.zshrc
 cabal install hoogle
 hoogle data
+chmod g-w ~/.ghc
+chmod g-w ~/.ghc/ghci.conf
--- haskell/install_xmonad.sh
+++ haskell/install_xmonad.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get install xmonad libghc-xmonad-dev libghc-xmonad-contrib-dev
 sudo apt-get install xmobar
+mkdir ~/.xmonad
+cp ./xmonad.hs ~/.xmonad/xmonad.hs
+cp ./xmobarrc ~/.xmobarrc
