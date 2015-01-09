80ee514773e46e2d74e623ca110a4882c8b26313
Brian Svoboda <svobodb@email.arizona.edu>
2015-01-09 13:39:45 -0700

    add pythonpy

--- python/install_py.sh
+++ python/install_py.sh
@@ -6,4 +6,4 @@ wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 # dependencies for matplotlib
 sudo apt-get install libagg-dev libjpeg8-dev libfreetype6-dev libgpng12-dev
-sudo pip install grin ipdb ipython matplotlib nose numexpr numpy pandas pypi-cli scipy scikit-learn astropy aplpy emcee jedi virtualenv sphinx
+sudo pip install grin ipdb ipython matplotlib nose numexpr numpy pandas pypi-cli scipy scikit-learn astropy aplpy emcee jedi virtualenv sphinx pythonpy
