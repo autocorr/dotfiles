5bad2bd715ccdc613cdc4e6e29580e4296ce47e1
Brian Svoboda <svobodb@email.arizona.edu>
2016-04-15 15:17:28 -0700

    changes

--- packages/install_cli.sh
+++ packages/install_cli.sh
@@ -3,4 +3,4 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 # console apps
 sudo apt-get install -y git tig htop ranger tmux vim zsh build-essential sshfs urlview fail2ban
-sudo apt-get install -y cmus mutt feh scrot irssi redshift rtorrent unclutter trash-cli mplayer slurm
+sudo apt-get install -y cmus mutt w3m feh scrot irssi redshift rtorrent unclutter trash-cli mplayer slurm
