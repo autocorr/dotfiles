ee2004eb23ed29125f990f499c64a706a9b84cf5
Brian Svoboda <svobodb@email.arizona.edu>
2016-08-26 11:45:22 -0700

    edit install themes script

--- theme/install_theme.sh
+++ theme/install_theme.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-cp -r ./Ambiance-Grey $HOME/.themes
-cp -r ./Faenza-Ambiance $HOME/.icons
+ICONSDIR=$HOME/.icons
+THEMESDIR=$HOME/.themes
 
-git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme-gtk/ $HOME/.icons
-mv $HOME/.icons/papirus-icon-theme-gtk/Papirus-GTK $HOME/.icons
-rm -rf $HOME/.icons/papirus-icon-theme-gtk
+cp -r ./Ambiance-Grey $THEMESDIR
+cp -r ./Faenza-Ambiance $ICONSDIR
+
+# install Papirus icon set
+git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme-gtk/ $ICONSDIR
+mv $HOME/.icons/papirus-icon-theme-gtk/Papirus-GTK $ICONSDIR
+rm -rf $ICONSDIR/papirus-icon-theme-gtk
