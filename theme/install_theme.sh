bf4cd2802e19a38630f789feb013d9d1c0c475c8
Brian Svoboda <svobodb@email.arizona.edu>
2016-08-26 11:29:23 -0700

    icon updates

--- theme/install_theme.sh
+++ theme/install_theme.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-cp -r ./Ambiance-Grey ~/.themes
-cp -r ./Faenza-Ambiance ~/.icons
-cp ./Inconsolata.otf ~/.fonts
+cp -r ./Ambiance-Grey $HOME/.themes
+cp -r ./Faenza-Ambiance $HOME/.icons
+
+git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme-gtk/ $HOME/.icons
+mv $HOME/.icons/papirus-icon-theme-gtk/Papirus-GTK $HOME/.icons
+rm -rf $HOME/.icons/papirus-icon-theme-gtk
