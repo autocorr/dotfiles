72313e851744342c4865c35ab8ee2c7de0fc8590
Brian Svoboda <svobodb@email.arizona.edu>
2017-01-09 14:32:29 -0700

    edit install packages

---
+++ packages/install_packages.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo apt update -y
+sudo apt upgrade -y
+
+# console
+sudo apt install -y git tig htop ranger tmux vim zsh build-essential sshfs urlview fail2ban mosh cmus mutt w3m feh scrot weechat rtorrent unclutter newsbeuter
+
+# gui
+sudo apt install -y firefox gimp inkscape steam stellarium transmission vlc
