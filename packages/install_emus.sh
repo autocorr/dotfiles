1707c729142968b5a03b6012e81946e80a0e8291
Brian Svoboda <svobodb@email.arizona.edu>
2017-01-09 14:32:08 -0700

    edit install packages

--- packages/install_emus.sh
+++ packages/install_emus.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+
 sudo apt update -y
 sudo apt upgrade -y
+
 sudo apt install -y desmume fceux mupen64plus pcsxr visualboyadvance-gtk zsnes
 echo ":: 'dolphin-emulator' requires manual install"
--- packages/install_gui.sh
+++ packages/install_gui.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+
 sudo apt update -y
 sudo apt upgrade -y
+
 echo ":: Installing main GUI packages"
 sudo apt install -y firefox gimp saods9 skype steam stellarium thunderbird transmission virtualbox vlc wine xchat
+
 echo ":: Installing auxillary GUI packages"
 sudo apt install -y unity-tweak-tool gnome-tweak-tool
 sudo apt install -y google-chrome-beta
+
 echo ":: google-earth, pycharm, acroread"
 echo ":: require manual install"
