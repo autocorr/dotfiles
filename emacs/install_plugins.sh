268311e5dec933fe83c511c017f40abef4df8535
Brian Svoboda <svobodb@email.arizona.edu>
2017-12-03 15:34:05 -0700

    new emacs config

--- emacs/install_plugins.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-EDIR=$HOME/.emacs.d
-wget https://raw.githubusercontent.com/JuliaLang/julia/master/contrib/julia-mode.el -O $EDIR/julia-mode.el
-git clone https://github.com/gabrielelanaro/emacs-for-python $EDIR/emacs-for-python
